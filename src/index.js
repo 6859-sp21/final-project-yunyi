763e09546d740ae5ba996868e0ff7af4a161f6af
Yunyi Zhu <yunyizhu@mit.edu>
2021-05-02 00:26:55 -0400

    set up structure

--- src/index.js
+++ src/index.js
@@ -2,19 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter, Route } from 'react-router-dom';
 import './index.scss';
-import App from './pages/App';
+
+// pages
+import AboutPage from "./pages/about";
 import BeingPage from "./pages/being";
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <HashRouter>
-    <Route exact path="/" component={App} />
+    <Route exact path="/" component={AboutPage} />
     <Route exact path="/being" component={BeingPage} />
   </HashRouter>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
