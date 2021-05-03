cb2de6a09e855a6fa105ee029ddd9bf4a6c824e1
Yunyi Zhu <yunyizhu@mit.edu>
2021-05-03 02:01:24 -0400

    third child visualization

--- src/pages/birth.js
+++ src/pages/birth.js
@@ -1,8 +1,34 @@
 import React from 'react';
 import Navbar from '../components/navbar';
-import { ResponsiveChoropleth } from '@nivo/geo'
-import SexRatioWorldData from '../data/sex-ratio-world.json';
+import { ResponsiveChoropleth } from '@nivo/geo';
+import { ResponsiveBar } from '@nivo/bar';
+
+
+
 import countries from "../data/world_countries.json";
+import SexRatioWorldData from '../data/sex-ratio-world.json';
+import SexRatioNumChildData from '../data/sex-ratio-num-child.json';
+
+const NewBornNumberJuxtaposition = () => (
+  <div id="new-born-number-visualization">
+    <div id="new-born-number-china">
+      <div className="caption">
+        China
+      </div>
+      <div className="number">
+        1.21 : 1
+      </div>
+    </div>
+    <div id="new-born-number-natural">
+      <div className="caption">
+        Natural Value
+      </div>
+      <div className="number">
+        1.05 : 1
+      </div>
+    </div>
+  </div>
+)
 
 const SexRatioWorld = () => (
   <ResponsiveChoropleth
@@ -15,7 +41,7 @@ const SexRatioWorld = () => (
     label="properties.name"
     valueFormat="0.3s"
     enableGraticule={false}
-    borderWidth={0.5}
+    borderWidth={0.3}
     borderColor="#152538"
     legends={[
       {
@@ -45,7 +71,67 @@ const SexRatioWorld = () => (
   />
 )
 
-
+const SexRatioByChildNumber = () => (
+  <ResponsiveBar
+    data={SexRatioNumChildData}
+    keys={[ 'boy', 'girl', 'girl_missing' ]}
+    indexBy="num_child"
+    padding={0.5}
+    margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
+    valueScale={{ type: 'linear' }}
+    indexScale={{ type: 'band', round: true }}
+    colors = { (bar) => ({'boy': 'lightblue', 'girl': 'pink', 'girl_missing': 'darkred' }[bar.id])}
+    borderColor={{ from: 'color', modifiers: [ [ 'darker', 1.6 ] ] }}
+    axisTop={null}
+    axisRight={null}
+    axisBottom={{
+      tickSize: 5,
+      tickPadding: 5,
+      tickRotation: 0,
+      legend: 'num_child',
+      legendPosition: 'middle',
+      legendOffset: 32
+    }}
+    axisLeft={{
+      tickSize: 5,
+      tickPadding: 5,
+      tickRotation: 0,
+      legend: 'sex',
+      legendPosition: 'middle',
+      legendOffset: -40
+    }}
+    labelSkipWidth={12}
+    labelSkipHeight={12}
+    labelTextColor={{ from: 'color', modifiers: [ [ 'darker', 1.6 ] ] }}
+    legends={[
+      {
+        dataFrom: 'keys',
+        anchor: 'bottom-right',
+        direction: 'column',
+        justify: false,
+        translateX: 120,
+        translateY: 0,
+        itemsSpacing: 2,
+        itemWidth: 100,
+        itemHeight: 20,
+        itemDirection: 'left-to-right',
+        itemOpacity: 0.85,
+        symbolSize: 20,
+        effects: [
+          {
+            on: 'hover',
+            style: {
+              itemOpacity: 1
+            }
+          }
+        ]
+      }
+    ]}
+    animate={true}
+    motionStiffness={90}
+    motionDamping={15}
+  />
+)
 
 const BirthPage = () => {
   return (
@@ -55,12 +141,30 @@ const BirthPage = () => {
         <div className="title">
           The Unwanted Daughter
         </div>
+
+        <div className="horizontal-line" />
         <div className="secondary-title">
           In 2009, the sex ratio of the new born babies was
         </div>
-        <div style={{"height": "450px"}}>
+        <NewBornNumberJuxtaposition />
+
+        <div className="horizontal-line" />
+        <div className="secondary-title">
+          This number was <b>the largest</b> among all countries in the world.
+        </div>
+
+        <div style={{"height": "450px", "margin": "60px"}}>
           <SexRatioWorld />
         </div>
+
+        <div className="horizontal-line" />
+        <div className="secondary-title">
+          Due to the one-child policy, the second or third child (illegal) faces even harsher sex selection.
+        </div>
+        <div style={{"height": "450px", "margin": "60px"}}>
+          <SexRatioByChildNumber />
+        </div>
+
 
       </div>
     </div>
