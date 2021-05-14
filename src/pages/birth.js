6181af86d1b3adfc4ad046b46db76738fa769d5a
Yunyi Zhu <yunyizhu@mit.edu>
2021-05-14 02:12:08 -0400

    outline, a bit

--- src/pages/birth.js
+++ src/pages/birth.js
@@ -4,24 +4,14 @@ import Navbar from '../components/navbar';
 import SexRatioByChildNumber from '../visualizations/birth-sex-ratio-by-child-number';
 import SexRatioWorld from "../visualizations/birth-sex-ratio-world";
 
-const NewBornNumberJuxtaposition = () => (
-  <div id="new-born-number-visualization">
-    <div id="new-born-number-china">
-      <div className="caption">
-        China
-      </div>
-      <div className="number">
-        1.21 : 1
-      </div>
-    </div>
-    <div id="new-born-number-natural">
-      <div className="caption">
-        Natural Value
-      </div>
-      <div className="number">
-        1.05 : 1
-      </div>
-    </div>
+const SexRatioNumber = () => (
+  <div style={{
+    fontSize: "150px",
+    textAlign: "center",
+    border: "5px solid black"
+
+  }}>
+    1.21: 1
   </div>
 )
 
@@ -41,51 +31,63 @@ const BirthPage = () => {
       <Navbar pageName="birth"/>
 
       <div className="page-contents">
-          <div className="visualization" >
-            {currentStepIndex===2 ?
-              <NewBornNumberJuxtaposition /> :
-              currentStepIndex === 3 ?
-                <div style={{"height": "450px", "margin": "60px"}}>
-                  <SexRatioWorld />
-                </div> :
-                currentStepIndex === 4 ?
-                  <div style={{"height": "450px", "margin": "60px"}}>
-                    <SexRatioByChildNumber />
-                  </div> :
-                  <div>
-                    <NewBornNumberJuxtaposition />
-                  </div>
-            }
-          </div>
+        <div className="visualization" style={{visibility:
+            currentStepIndex===2 ? "visible" : "hidden"}}>
+          <SexRatioNumber />
+        </div>
+        <div className="visualization" style={{visibility: currentStepIndex===3 ? "visible" : "hidden"}}>
+          <SexRatioWorld />
+        </div>
+        <div className="visualization" style={{visibility: currentStepIndex===4 ? "visible" : "hidden"}}>
+          <SexRatioByChildNumber />
+        </div>
+
 
         <div className="accompany-text-all">
           <Scrollama onStepEnter={onStepEnter} >
             <Step data={1}>
-              <div className="title">
+              <h1>
                 The Unwanted Daughter
-              </div>
+              </h1>
             </Step>
             <Step data={2}>
-              <div className="accompany-text">
-                <div className="secondary-title">
-                  In 2009, the sex ratio of the new born babies was
-                </div>
+              <div className="text-block">
+                <p>
+                  In 2009, the sex ratio of the new born babies in China was <b>1.21: 1</b>.
+                </p>
               </div>
             </Step>
             <Step data={3}>
-              <div className="accompany-text">
-                <div className="secondary-title">
-                  This number was <b>the largest</b> among all countries in the world.
-                </div>
+              <div className="text-block">
+                <p>
+                  This means, 121 boys are born with every 100 girls.
+                </p>
               </div>
             </Step>
+
             <Step data={4}>
-              <div className="accompany-text">
-                <div className="secondary-title">
-                  Due to the one-child policy, the second or third child (illegal) faces even harsher sex selection.
-                </div>
+              <div className="text-block">
+                <p>
+                  This number was the highest among the world, and
+                  much higher than the world average, 1.05: 1.
+                </p>
               </div>
             </Step>
+
+            {/*<Step data={3}>*/}
+            {/*  <div className="text-block">*/}
+            {/*    <p>*/}
+            {/*      This number was <b>the largest</b> among all countries in the world.*/}
+            {/*    </p>*/}
+            {/*  </div>*/}
+            {/*</Step>*/}
+            {/*<Step data={4}>*/}
+            {/*  <div className="text-block">*/}
+            {/*    <p>*/}
+            {/*      Due to the one-child policy, the second or third child (illegal) faces even harsher sex selection.*/}
+            {/*    </p>*/}
+            {/*  </div>*/}
+            {/*</Step>*/}
           </Scrollama>
         </div>
 
---
+++ src/visualizations/birth-sex-ratio-dot-plot.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SexRatioDotPlot = () => (
+  <div>
+    <div>
+      {
+
+      }
+    </div>
+  </div>
+)
